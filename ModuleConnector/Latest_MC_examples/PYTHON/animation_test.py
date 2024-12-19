adc148f3bc6ef1873c84253667f20817408c0970
Gustav <gtc@vitalthings.com>
2024-12-19 21:02:21 +0100

    use func animation instead

--- ModuleConnector/Latest_MC_examples/PYTHON/animation_test.py
+++ ModuleConnector/Latest_MC_examples/PYTHON/animation_test.py
@@ -1,28 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import ArtistAnimation
+import matplotlib.animation as animation
 
 # Create a figure and axis
 fig, ax = plt.subplots()
 
-# Function to create frames for animation
-def create_frames(num_frames):
-    frames = []
-    for _ in range(num_frames):
-        data = np.random.rand(10, 10)
-        im = ax.imshow(data, cmap='viridis')
-        frames.append([im])  # Each frame contains a list of artists (images)
-        colorbar.update_normal(im)  # Update colorbar to reflect new data
-    return frames
-
 # Create initial data and colorbar
 data = np.random.rand(10, 10)
-im = ax.imshow(data, cmap='viridis')
-colorbar = fig.colorbar(im, ax=ax)
+im = ax.imshow(data)
+# colorbar = fig.colorbar(im, ax=ax)
 
-# Create frames for the animation
-frames = create_frames(num_frames=100)
+# Function to update the frame
+def update(frame):
+    data = np.random.rand(10, 10)
+    im.set_array(data)
+    # colorbar.update_normal(im)
+    return im,
 
 # Create and display the animation
-ani = ArtistAnimation(fig, frames, interval=50, blit=True)
+ani = animation.FuncAnimation(fig, update, frames=100, interval=50, blit=True)
 plt.show()
