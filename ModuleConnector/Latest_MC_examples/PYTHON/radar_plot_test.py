09cd2dfe063dd1d71265fcd3cab244fe3ea57934
Gustav <gtc@vitalthings.com>
2024-12-22 09:44:25 +0100

    plot fft

--- ModuleConnector/Latest_MC_examples/PYTHON/radar_plot_test.py
+++ ModuleConnector/Latest_MC_examples/PYTHON/radar_plot_test.py
@@ -2,24 +2,21 @@ import numpy as np
 import cv2
 import time
 
-radar_matrix = np.load('data/easy2.npy')
+radar_matrix = np.load('data/hard2.npy')
 
-window_size = 50
+FREQUENCY_CUTOFF = 2
+window_size = 100
 FPS = 17
+FFT_INTERVAL = FPS # calculate FFT every second
 
 # radar_window = radar_matrix[:window_size]
 radar_window = np.zeros((window_size, radar_matrix.shape[1]), dtype=np.complex128)
-
-prev_time = time.time()
-prev_frame = 0
 
 # Adjust this value to increase or decrease the image size
 scale_factor = 500/window_size
 width_to_height_ratio = 2
 
-frame_index = 0
-
-def plot_radar_window(window, scale_factor=1, width_to_height_ratio=1):
+def plot_radar_window(window, scale_factor=1, width_to_height_ratio=1, name='radar'):
     img = np.abs(window).T
     img = cv2.normalize(img, None, 0.0, 1.0, cv2.NORM_MINMAX)
     img = np.float32(img)
@@ -34,9 +31,34 @@ def plot_radar_window(window, scale_factor=1, width_to_height_ratio=1):
     img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_NEAREST)
 
     # Display the image
-    cv2.imshow('Radar Window', img)
+    cv2.imshow(name, img)
 
     return img
+
+def get_fft_matrix(radar_matrix):
+    radar_matrix_fft = np.fft.fft(radar_matrix, axis=0)
+    num_frames = radar_matrix.shape[0]
+    sample_spacing = 1.0 / FPS
+    freqs = np.fft.fftfreq(num_frames, sample_spacing)
+
+    # Only keep positive frequencies and skip 0
+    # TODO: why do we remove 0? check pySDR for answer
+    positive_freqs = freqs > 0
+    radar_matrix_fft = radar_matrix_fft[positive_freqs, :]
+    freqs = freqs[positive_freqs]
+    freqs = freqs[freqs < FREQUENCY_CUTOFF]
+    radar_matrix_fft = radar_matrix_fft[:len(freqs), :]
+    return radar_matrix_fft, freqs
+
+def get_fft_matrix_max(fft_matrix, freqs):
+    abs_matrix = np.abs(fft_matrix)
+    max_value = np.max(abs_matrix)
+    max_index = np.argmax(abs_matrix)
+    row, col = np.unravel_index(max_index, abs_matrix.shape)
+    freq = freqs[row]
+    bin = col
+    print(f"Max value: {max_value} at freq {freq} and bin {bin}")
+    return freq, bin
 
 def plot_show(delay=1):
     if cv2.waitKey(delay) & 0xFF == ord('q'):
@@ -59,7 +81,8 @@ class FPSCounter:
             self.prev_frame += 1
 
 
-fps = FPSCounter()
+fps_cntr = FPSCounter()
+frame_index = 0
 
 while True:
     radar_window = np.roll(radar_window, -1, axis=0)
@@ -69,9 +92,14 @@ while True:
         break
 
     # calculate fps each second
-    fps.update()
+    fps_cntr.update()
 
-    plot_radar_window(radar_window, scale_factor, width_to_height_ratio)
+    plot_radar_window(radar_window, scale_factor, width_to_height_ratio, name='radar')
+
+    if frame_index % FFT_INTERVAL == 0:
+        fft_matrix, freqs = get_fft_matrix(radar_window)
+        fft_matrix_max = get_fft_matrix_max(fft_matrix, freqs)
+        plot_radar_window(fft_matrix.T, scale_factor*10, name='fft')
     plot_show(50)
 
 cv2.destroyAllWindows()
